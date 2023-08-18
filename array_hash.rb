91932f8b287276c6454397d227ebbf95cb65c9cb
allivingston <a.e.krlkv@gmail.com>
2023-08-19 01:23:52 +0300

    first_homework_fix

--- array_hash.rb
+++ array_hash.rb
@@ -1,2 +1,7 @@
-array = [1,2,3,4,5]
-{min: array.min, max: array.max}
+def hash_array(input_array)
+  hash={min: input_array.min, max: input_array.max}
+end
+
+array = [1, 2, 3, 4, 5]
+hash = hash_array(array)
+puts "Хэш с минимальным и максимальным числом: #{hash}"
--- array_reverse.rb
+++ array_reverse.rb
@@ -1,2 +1,7 @@
-array = [1,2,3,4,5]
-array.reverse
+def reverse_array(input_array)
+  reverse=input_array.reverse
+end
+
+array = [1, 2, 3, 4, 5]
+reverse = reverse_array(array)
+puts "Инвертированный массив: #{reverse}"
